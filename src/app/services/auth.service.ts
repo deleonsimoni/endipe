44802e14128ea750c989e4838905c67803b0dee2
DeleonxD <deleonsimoni@gmail.com>
2019-08-09 01:18:31 -0300

    acertos login

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,8 @@
 import { Injectable, Inject } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import * as jwt_decode from 'node_modules/jwt-decode';
 
+const TOKEN_KEY = '_edt';
 @Injectable({
   providedIn: 'root'
 })
@@ -20,9 +22,30 @@ export class AuthService {
   }
 
   me() {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem(TOKEN_KEY);
     const header = new HttpHeaders().set('Authorization', 'Bearer ' + token);
     return this.http.get(`${this.baseUrl}/auth/me`, { headers: header });
+  }
+
+  setUser(user, token): void {
+    window.localStorage.setItem(TOKEN_KEY,  token);
+    (<any> window).user = user;
+  }
+
+  public getUser(): string {
+    return (<any>window).user;
+  }
+
+  public getToken(): string {
+    return localStorage.getItem(TOKEN_KEY);
+  }
+
+  public getDecodedAccessToken(token: string): any {
+    try{
+        return jwt_decode(token);
+    } catch(Error){
+        return null;
+    }
   }
 
 }
