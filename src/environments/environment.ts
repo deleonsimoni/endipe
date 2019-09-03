109042f6916d856488a874ed5a90d5f037792525
DeleonxD <deleonsimoni@gmail.com>
2019-09-03 00:24:28 -0300

    acerto host

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,7 @@
 export const environment = {
   production: false,
   // host: 'http://xxendiperio2020.com.br/api'
-  //host: 'http://localhost:8080/api'
-  host: 'https://breduka.localtunnel.me'
+  host: 'http://localhost:8080/api'
 };
 
 /*
