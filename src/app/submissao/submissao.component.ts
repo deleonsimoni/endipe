a3b8856a2cf59f060c7c1fed2261e9d82777fe91
DeleonxD <deleonsimoni@gmail.com>
2019-09-04 23:10:00 -0300

    acerto modalidade

--- src/app/submissao/submissao.component.ts
+++ src/app/submissao/submissao.component.ts
@@ -64,11 +64,12 @@ export class SubmissaoComponent implements OnInit {
     const vm = this;
     this.authService.refresh().subscribe((res: any) => {
       this.user = res.user;
-      /*if (this.user.modalityId) {
-        this.user.modalityId.forEach(function (value) {
-          vm.modalidadesUsuario.push(vm.modalidades.find(item => item.id == value));
-        });
-      }*/
+      this.modalidadesUsuario = this.modalidades;
+      /* if (this.user.modalityId) {
+         this.user.modalityId.forEach(function (value) {
+           vm.modalidadesUsuario.push(vm.modalidades.find(item => item.id == value));
+         });
+       }*/
       this.carregando = false;
     });
 
