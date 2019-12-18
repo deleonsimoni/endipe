81d98de94865cd73f409d146e5e9605463c0ed3b
DeleonxD <deleonsimoni@gmail.com>
2019-12-18 01:10:39 -0200

    correções

--- src/app/admin/components/subscribers-data/subscribers-data.component.ts
+++ src/app/admin/components/subscribers-data/subscribers-data.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { AdminService } from '../../admin.service';
+import { DownloadFileService } from 'src/app/services/download-file.service';
 
 @Component({
   // tslint:disable-next-line: component-selector
@@ -10,20 +11,51 @@ import { AdminService } from '../../admin.service';
 export class SubscribersDataComponent implements OnInit {
 
   @Input() subscribed: any;
-
-  public categories = [
-    { id: 1, name: 'Estudantes de curso Normal/EM' },
-    { id: 2, name: 'Estudantes de Graduação' },
-    { id: 3, name: 'Estudantes de Pós-Graduação' },
-    { id: 4, name: 'Profissionais da Educação Básica' },
-    { id: 5, name: 'Profissionais da Educação Superior' }
-  ];
+  public carregando = false;
+  public carregandoTrabalhos = false;
+  public works;
 
   constructor(
-    private adminService: AdminService
+    private adminService: AdminService,
+    private downloadService: DownloadFileService
   ) { }
 
   ngOnInit() {
+    if (this.subscribed.works.length > 0) {
+      this.userWorks(this.subscribed.works);
+    }
+  }
+
+  public userWorks(userWorksId) {
+    this.works = [];
+    this.carregandoTrabalhos = true;
+
+    if (userWorksId) {
+      userWorksId.forEach(workId => {
+        this.adminService.retrieveUserWorks(workId)
+          .subscribe((res: any) => {
+            this.carregandoTrabalhos = false;
+            this.works.push(res);
+          }, err => {
+            this.carregandoTrabalhos = false;
+            console.log(err);
+          });
+      });
+    }
+
+  }
+
+  public download(nameFile) {
+    const vm = this;
+    function sucessoDownload() {
+      vm.carregando = false;
+    }
+    function falhaDownload(err) {
+      this.toastr.error('Erro ao relizar download.', 'Erro: ');
+      vm.carregando = false;
+    }
+    this.carregando = true;
+    this.downloadService.getFile(nameFile, sucessoDownload, falhaDownload);
   }
 
   public confirmPayment(user) {
@@ -44,8 +76,44 @@ export class SubscribersDataComponent implements OnInit {
       });
   }
 
+  public retrieveModality(id) {
+    return this.modalities.filter(element => element.id === id)[0];
+  }
+
+  public retrieveEixo(id) {
+    return this.eixos.filter(element => element.id === id)[0];
+  }
+
   public retrieveCategories(id) {
     return this.categories.filter(element => element.id === id)[0];
   }
+
+  public modalities = [
+    { id: 1, name: 'Convidado de sessão especial' },
+    { id: 2, name: 'Mediador de roda de conversa' },
+    { id: 3, name: 'Expositor de pôster' },
+    { id: 4, name: 'Mediador de minicurso' },
+    { id: 5, name: 'Coordenador e/ou expositor de painel' },
+    { id: 6, name: 'Simposista' },
+    { id: 7, name: 'Ouvinte' }
+  ];
+
+  public eixos = [
+    { id: 1, name: 'Formação docente' },
+    { id: 2, name: 'Currículo e avaliação' },
+    { id: 3, name: 'Direitos humanos, Interculturalidade e Religiões' },
+    { id: 4, name: 'Nova epistemologia, Diferença, Biodiversidade, Democracia e Inclusão' },
+    { id: 5, name: 'Educação, Comunicação e Técnologia' },
+    { id: 6, name: 'Infâncias, Juventudes e Vida Adulta' }
+  ];
+
+  public categories = [
+    { id: 1, name: 'Estudantes de curso Normal/EM' },
+    { id: 2, name: 'Estudantes de Graduação' },
+    { id: 3, name: 'Estudantes de Pós-Graduação' },
+    { id: 4, name: 'Profissionais da Educação Básica' },
+    { id: 5, name: 'Profissionais da Educação Superior' }
+  ];
+
 
 }
