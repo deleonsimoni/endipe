9564e39ba23fb42b7961174beb9da6d8616d8123
iuriian <iuriian@hotmail.com>
2020-03-03 02:21:29 -0300

    implementação de cards, botoes de filtro por data e ordenação por hora

--- src/app/programacao/programacao.component.ts
+++ src/app/programacao/programacao.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 import { PROGRAMACOES } from '../declarations';
+import { AdminService } from '../admin/admin.service';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-programacao',
@@ -10,10 +13,23 @@ import { PROGRAMACOES } from '../declarations';
 export class ProgramacaoComponent implements OnInit {
 
   public programacoes = PROGRAMACOES;
+  public schedules$: Observable<any[]>;
+  public daySelected = '14/07';
 
-  constructor() { }
+  constructor(
+    private adminService: AdminService
+  ) { }
 
   ngOnInit() {
+    this.schedules$ = this.adminService.retrieveSchedules()
+      .pipe(map(res => this.orderByHour(res)));
   }
 
+  private orderByHour(data) {
+    return data.sort((a, b) => Number(a.hour.replace(':', '')) - Number(b.hour.replace(':', '')));
+  }
+
+  public currentTab($event) {
+    console.log($event.tab.textLabel);
+  }
 }
