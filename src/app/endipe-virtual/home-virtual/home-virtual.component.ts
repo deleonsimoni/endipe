638f019a7d7fb928f914556442e9e3ebe01604cb
Deleon Simoni <deleon.simoni@gmail.com>
2020-07-20 01:58:02 -0300

    acessibilidade menu

--- src/app/endipe-virtual/home-virtual/home-virtual.component.ts
+++ src/app/endipe-virtual/home-virtual/home-virtual.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home-virtual',
@@ -15,7 +16,118 @@ export class HomeVirtualComponent implements OnInit {
   panelOpenState = false;
   panelOpenState2 = false;
 
-  constructor() { }
+  postAuthorEmail = 'jan.kanty.pawelski@gmail.com'
+
+  comments = [
+    {
+      'id': 1,
+      'author': {
+        'name': 'Jan-Kanty Pawelski',
+        'email': 'jan.kanty.pawelski@gmail.com',
+        'website': 'pawelski.io',
+        },
+      'content': 'I made it! My awesome angular comment system. What do you think?',
+      'loved': false
+    },
+    {
+      'id': 2,
+      'author': {
+        'name': 'Tomasz Jakut',
+        'email': 'comandeer@comandeer.pl',
+        'website': 'comandeer.pl',
+        },
+      'content': 'Nice looking. Good job dude ;)',
+      'loved': true,
+    },
+    {
+      'id': 3,
+      'author': {
+        'name': 'Jan-Kanty Pawelski',
+        'email': 'jan.kanty.pawelski@gmail.com',
+        'website': 'pawelski.io',
+        },
+      'content': '<span class="reply">@Tomasz Jakut</span> Thanks man. I tried hard.',
+      'loved': false,
+    },
+    {
+      'id': 4,
+      'author': {
+        'name': 'Grzegorz Bąk',
+        'email': 'szary.elf@gmail.com',
+        'website': 'gregbal.com',
+        },
+      'content': 'Third! Amazing system man! By the way check my new website: <a href="//gregbak.com">http://gregbak.com</a>.',
+      'loved': false,
+    }
+  ]
+
+  newComment = {
+      'id': 0,
+      'author': {
+        'name': '',
+        'email': '',
+        'website': '',
+      },
+      'content': '',
+      'loved': false,
+  }
+
+
+  parseContent(content) {
+    return this.sanitizer.bypassSecurityTrustHtml(content);
+  };
+
+  isAuthor(email) {
+    return email === this.postAuthorEmail
+  } 
+
+  love (commentId){
+    this.comments.forEach(comment => {
+      if (comment.id == commentId)
+        comment.loved = !comment.loved
+    });      
+  }
+
+  reply(author) {
+      if (!this.newComment.content)
+      this.newComment.content = ''
+
+      if (this.newComment.content.search('@' + author + '@') == -1) {
+        if (this.newComment.content[0] == '@')
+          this.newComment.content = ', ' + this.newComment.content
+        else
+          this.newComment.content = ' ' + this.newComment.content
+
+         this.newComment.content = '@' + author + '@' + this.newComment.content
+      }
+  }
+
+  addNewComment() {
+      if( this.newComment.author.name.length > 0 && 
+          this.newComment.author.email.length > 0 &&
+          this.newComment.content.length > 0){
+
+            this.newComment.id = this.comments.length + 1
+    
+            this.newComment.author.website = this.newComment.author.website.replace(/https?:\/\/(www.)?/, '')
+    
+            this.newComment.content = this.newComment.content.replace(/(@[^@.]+)@/, '<span class="reply">$1</span>')
+            this.newComment.content = this.newComment.content.replace(/https?:\/\/(www.)?([a-zA-Z0-9\-_]+\.[a-zA-Z0-9]+)/, '<a href="//$2">$2</a>')
+    
+            this.newComment.loved = false
+    
+            this.comments.push( this.newComment);
+
+        }
+
+
+
+  }
+
+      
+
+
+  constructor(private sanitizer: DomSanitizer) { }
 
   ngOnInit() {
     this.countDownDate = new Date("Jan 5, 2021 15:37:25").getTime();
