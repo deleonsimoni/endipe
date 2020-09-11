8e88c455887ce82ccf0e259a8b592972a5064c3c
Deleon Simoni <deleon.simoni@gmail.com>
2020-09-11 12:14:59 -0300

    acertos visuais

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  //host: 'http://18.228.5.115:8080/api'
-  'host': "http://localhost:8080/api"
+  host: 'http://18.228.5.115:8080/api'
+  //host: "http://localhost:8080/api"
 };
 
 /*
