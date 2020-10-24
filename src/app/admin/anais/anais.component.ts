bfe862b135bcf5d8617ee3d6d0958ad2af6ee266
Deleon Simoni <deleon.simoni@gmail.com>
2020-10-24 00:53:50 -0300

    disable outside click

--- src/app/admin/anais/anais.component.ts
+++ src/app/admin/anais/anais.component.ts
@@ -2,8 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { AnaisService } from 'src/app/services/anais.service';
-import { MatDialog } from '@angular/material';
+import { MatDialog, MatSnackBar } from '@angular/material';
 import { ModalAnaisComponent } from '../modals/modal-anais/modal-anais.component';
+import { ConfirmationDialogComponent } from '../modals/confirmation-dialog/confirmation-dialog.component';
 
 @Component({
   selector: 'app-anais',
@@ -17,6 +18,8 @@ export class AnaisComponent implements OnInit {
     private builder: FormBuilder,
     private anaisService: AnaisService,
     private dialog: MatDialog,
+    private snackBar: MatSnackBar
+
   ) {
   }
 
@@ -53,11 +56,36 @@ export class AnaisComponent implements OnInit {
       });
   }
 
+  public confirmDeleteAnais(anais) {
+    const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
+      data: {
+        message: 'Tem certeza que deseja remover o e-book?',
+        buttonText: {
+          ok: 'Sim',
+          cancel: 'Cancelar'
+        }
+      }
+    });
+    const snack = this.snackBar.open('Caso clique em sim, o autor será removido do trabalho');
+
+    dialogRef.afterClosed().subscribe((confirmed: boolean) => {
+      snack.dismiss();
+      if (confirmed) {
+        this.deletar(anais);
+        this.snackBar.open('Removendo e-book', 'Fechar', {
+          duration: 2000,
+        });
+      }
+    });
+
+  }
+
   public register(anal) {
     if(anal == 0) {
       anal = null;
     }
     const dialogRef = this.dialog.open(ModalAnaisComponent, {
+      disableClose: true,
       data: {
         anal: anal
       }
