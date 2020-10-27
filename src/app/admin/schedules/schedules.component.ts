299058554505b80b0c82055a596521283f5b5715
Deleon Simoni <deleon.simoni@gmail.com>
2020-10-27 00:36:39 -0300

    liberacao link zoom

--- src/app/admin/schedules/schedules.component.ts
+++ src/app/admin/schedules/schedules.component.ts
@@ -15,7 +15,7 @@ import { Router } from "@angular/router";
 export class SchedulesComponent implements OnInit {
   public schedules$: BehaviorSubject<any[]> = new BehaviorSubject<any[]>([]);
   public programacoes = SCHEDULE_TYPE;
-  public days = ["24/10", "25/10", "31/10", "01/11", "02/11", "03/11", "04/11", "05/11", "06/11", "07/11", "08/11", "09/11", "10/11", "11/11", "12/11"];
+  public days = ["29/10", "30/10", "31/10", "01/11", "02/11", "03/11", "04/11", "05/11", "06/11", "07/11", "08/11", "09/11", "10/11", "11/11", "12/11"];
   public daySelected$: BehaviorSubject<string> = new BehaviorSubject<string>("25/10");
   public label = "Abertura";
   public typeId: any;
--- src/app/endipe-virtual/home-virtual/home-virtual.component.ts
+++ src/app/endipe-virtual/home-virtual/home-virtual.component.ts
@@ -12,11 +12,7 @@ import { ModalBemVindoComponent } from 'src/app/modal-bem-vindo/modal-bem-vindo.
 })
 export class HomeVirtualComponent implements OnInit {
 
-  countDownDate;
-  days; 
-  hours;
-  minutes;
-  seconds;
+  
   carregando = false;
   user;
   comments;
@@ -31,35 +27,15 @@ export class HomeVirtualComponent implements OnInit {
   ngOnInit() {
 
     this.retrieveUser();
-
+/*
     if(!localStorage.getItem("bemVindoEndipe")){
 
       const dialogRef = this.dialog.open(ModalBemVindoComponent, {});
 
       localStorage.setItem("bemVindoEndipe", "1");
 
-    }
+    }*/
 
-    this.countDownDate = new Date("Oct 29, 2020 00:00:00").getTime();
-
-    setInterval(() => {
-
-        var now = new Date().getTime();
-      
-        // Find the distance between now an the count down date
-        let distance = this.countDownDate - now;
-        
-        // Time calculations for days, hours, minutes and seconds
-        this.days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        this.hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        this.minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        this.seconds = Math.floor((distance % (1000 * 60)) / 1000);
-        // If the count down is over, write some text 
-        /*if (distance < 0) {
-          clearInterval(this.countdownfunction);
-          document.getElementById("demo").innerHTML = "EXPIRED";
-        }*/
-      }, 1000);
 
   }
 
